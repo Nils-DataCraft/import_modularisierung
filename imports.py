5ff594795563537ab096ec06b3f51d16d21f2bac
Nils-DataCraft <nils@data-craft.de>
2023-08-01 12:52:53 +0200

    erster commit

---
+++ imports.py
@@ -0,0 +1,13 @@
+#!python3
+
+# Achtung: Je mehr imports, desto längere Ladezeit und desto
+#          vollerer Namespace (mehr Namen auf die man achten muss)
+
+import os, sys, re, datetime
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+from ipywidgets import interact
